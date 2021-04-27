d520c2abf2de6ac3ec2631e84202839049896fb0
Anastasiya Novik <nastiuchanoivk00@mail.ru>
2021-04-27 17:54:43 +0300

    Добавьте файлы проекта.

---
+++ HOTEL_PROJECT/OracleDatabaseConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOTEL_PROJECT
+{
+    class OracleDatabaseConnection
+    {
+        public static string str = "Data Source = (DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.56.104)" +
+               "(PORT = 1521)))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = NAD_PDB.be.by)));User Id=admin;Password=Administrator1;";
+    }
+}
---
+++ HOTEL_PROJECT/Users/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOTEL_PROJECT.Users
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Secondname { get; set; }
+        public double Salary { get; set; }
+        public string PhoneNumber { get; set; }
+        public int PositionId { get; set; }
+        public DateTime DateOfHiring { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
